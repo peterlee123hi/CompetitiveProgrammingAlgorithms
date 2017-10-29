82609e5f7f1334cafc52141f2e573712dc4ae690
Peter Lee <peter.lee@berkeley.edu>
2017-10-29 02:23:51 -0700

    Updated unionfind

--- UnionFind.cpp
+++ UnionFind.cpp
@@ -1,4 +1,4 @@
-#include <vector>
+#include <bits/stdc++.h>
 #define rep(i,a,n) for(int i=a;i<n;i++)
 using namespace std;
 typedef vector<int> vi;
@@ -23,10 +23,10 @@ public:
 	void unionSet(int i, int j) {
 		if(!isSameSet(i, j)) {
 			int x = findSet(i), y = findSet(j);
-			if(rank[x] > rank[y]) {
-				p[y] = p[x];
-			} else {
+			if(rank[x] < rank[y]) {
 				p[x] = p[y];
+			} else {
+				p[y] = p[x];
 				if(rank[x] == rank[y])
 					rank[y]++;
 			}
